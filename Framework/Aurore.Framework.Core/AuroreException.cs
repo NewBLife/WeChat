137be027940317e2607fa722f7fc0ec943b12d4d
Michael <490811331@qq.com>
2016-04-15 14:38:41 +0800

    Update

---
+++ Framework/Aurore.Framework.Core/AuroreException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aurore.Framework.Core
+{
+    [Serializable]
+    public class AuroreException : Exception
+    {
+        public int ErrorCode { get; set; }
+
+        public AuroreException() : this(-10000)
+        {
+        }
+
+        public AuroreException(int errorCode) : this(errorCode, "不明确的异常")
+        {
+
+        }
+
+        public AuroreException(int errorCode, string message) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public AuroreException(Exception inner) : this("不明确的异常", inner)
+        {
+        }
+
+        public AuroreException(string message, Exception inner) : this(-10000, message, inner)
+        {
+        }
+        public AuroreException(int errorCode, string message, Exception inner) : base(message, inner)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ Framework/Aurore.Framework.Core/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Aurore.Framework.Core
+{
+    public interface ILogger
+    {
+
+        void WriteLog(string msg);
+        void WriteLog(string errorType,string msg);
+        void WriteLog(string errorType,string msg,AuroreException ex);
+    }
+}
